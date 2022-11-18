5c71f3e08c3e0eaff77d9e3f823a38f2dd5a33c6
Seamus Browne <31517569+SeamusBrowne@users.noreply.github.com>
2022-11-18 01:29:09 +0000

    Update AddCard.js

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,1 +1,51 @@
+import React, { useState } from "react";
 
+// import { X } from "react-feather";
+
+function AddCard(props) {
+  const [isEditable, setIsEditable] = useState(false);
+  const [inputText, setInputText] = useState(props.defaultValue || "");
+
+  const submission = (e) => {
+    e.preventDefault();
+    if (inputText && props.onSubmit) {
+      setInputText("");
+      props.onSubmit(inputText);
+    }
+    setIsEditable(false);
+  };
+
+  return (
+    <div className="add_card">
+      {isEditable ? (
+        <form
+          className={`add_card_edit ${props.editClass ? props.editClass : ""}`}
+          onSubmit={submission}
+        >
+          <input
+            type="text"
+            value={inputText}
+            placeholder={props.placeholder || props.text}
+            onChange={(event) => setInputText(event.target.value)}
+            autoFocus
+          />
+          <div className="add_card_edit_footer">
+            <button type="submit">{props.buttonText || "Add"}</button>
+            <X onClick={() => setIsEditable(false)} className="closeIcon" />
+          </div>
+        </form>
+      ) : (
+        <p
+          className={`add_card_text ${
+            props.displayClass ? props.displayClass : ""
+          }`}
+          onClick={() => setIsEditable(true)}
+        >
+          {props.text}
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default AddCard;
