f734dc8fa9a89b3f08d5221c7c537611bdf42126
Seamus Browne <31517569+SeamusBrowne@users.noreply.github.com>
2022-11-21 01:18:34 +0000

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,11 +9,12 @@ function Card(props) {
       <div
         className="card"
         draggable
-        onDragEnd={() => props.dragEnded(props.columnId, id)}
-        onDragEnter={() => props.dragEntered(props.columnId, id)}
+        onDragEnd={() => props.dragTo(props.columnId, id)}
+        onDragEnter={() => props.dragFrom(props.columnId, id)}
       >
 
         <div className="card_title">{title}</div>
+        <div className="card_footer"></div>
       </div>
     </>
   );
